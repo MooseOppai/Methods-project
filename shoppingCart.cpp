bd98216279176df41bf198b58b1bf9b1721253ca
niahj <62308696+niahj@users.noreply.github.com>
2022-07-25 17:53:36 -0500

    Add files via upload

---
+++ shoppingCart.cpp
@@ -0,0 +1,24 @@
+#include "shoppingCart.h"
+
+shoppingCart::shoppingCart() {
+    int cartID;
+    int productID;
+    productName = "";
+    double sum = 0.0;
+    
+    
+}
+
+shoppingCart::~shoppingCart() {
+    
+    
+}
+
+
+void addItem();
+void removeItem();
+void viewCartDetails();
+
+int itemQuantity;
+double total();
+
